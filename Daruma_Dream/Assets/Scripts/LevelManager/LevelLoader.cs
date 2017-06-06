b9e00dfce28bfd7a05ef1434fbf5f24cd7e0d12d
as <asd>
2017-06-06 10:09:35 +0200

    LevelLoader

---
+++ Daruma_Dream/Assets/Scripts/LevelManager/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+
+    private bool _playerInZone;
+
+    public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+
+        _playerInZone = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+
+            Debug.Log("Nya");
+
+        }
+
+    }
+}
