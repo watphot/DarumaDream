51f99a42d6ad97c569f512360034ba90c1fd42ac
SAM <stevenmejiasanchez@gmail.com>
2017-06-09 00:47:30 +0200

    Main Menu

---
+++ Daruma_Dream/Assets/Scripts/LevelManager/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startLevel;
+
+    public string levelSelect;
+
+    //public int playerLives;
+
+    public void NewGame()
+    {
+
+        Application.LoadLevel(startLevel);
+
+    }
+
+    public void LevelSelect()
+    {
+
+        Application.LoadLevel(levelSelect);
+
+    }
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
+    }
+
+}
