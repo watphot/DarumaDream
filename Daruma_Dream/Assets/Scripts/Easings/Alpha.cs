ffbef81a248cf184ee281c69cc876f5ad1f9ebad
asd <asd>
2017-06-15 10:03:03 +0200

    Easings

---
+++ Daruma_Dream/Assets/Scripts/Easings/Alpha.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Alpha : MonoBehaviour {
+
+    public float iniAlpha;
+    public float finalAlpha;
+    public float framesDuration;
+    public float framesCounter;
+    public Image myImage;
+    public AnimationCurve hyb;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myImage = GetComponent<Image>();
+        iniAlpha = myImage.color.a;
+        framesDuration = 3.5f;
+        framesCounter = 0;
+        Time.timeScale = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        framesCounter += Time.deltaTime;
+                
+        myImage.color = new Color(myImage.color.r, myImage.color.g, myImage.color.b, Mathf.Lerp(iniAlpha, finalAlpha, hyb.Evaluate(framesCounter / framesDuration)));
+
+        if(framesCounter >= framesDuration)
+        {
+
+            framesCounter = framesDuration;
+
+        }
+    }
+}
---
+++ Daruma_Dream/Assets/Scripts/Easings/Rename.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rename : MonoBehaviour {
+
+    public Vector2 initValue;
+    public float framesDuration;
+    public float framesCounter;
+    public Image myImage;
+    public GameObject finalPosition;
+    public AnimationCurve hyb;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myImage = GetComponent<Image>();
+        initValue = myImage.rectTransform.position;
+        framesDuration = 5f;
+        framesCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            framesCounter += Time.deltaTime;
+
+            myImage.rectTransform.position = Vector2.Lerp(initValue, finalPosition.transform.position, hyb.Evaluate(framesCounter / framesDuration));
+
+            if(framesCounter >= framesDuration)
+            {
+
+                framesCounter = framesDuration;
+
+            }
+    }
+}
+
